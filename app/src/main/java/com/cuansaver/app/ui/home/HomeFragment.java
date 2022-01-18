b1d84d3c643892b9a731e50a359ef3a3b3b6d937
Cornelius Tantius <corneliustantius.ct@gmail.com>
2022-01-18 22:06:41 +0700

    update

--- app/src/main/java/com/cuansaver/app/ui/home/HomeFragment.java
+++ app/src/main/java/com/cuansaver/app/ui/home/HomeFragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,11 +15,22 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.cuansaver.app.R;
 import com.cuansaver.app.databinding.FragmentHomeBinding;
+import com.cuansaver.app.model.Data;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
     private FragmentHomeBinding binding;
+    private FirebaseDatabase database;
+    private DatabaseReference ref;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -27,12 +39,12 @@ public class HomeFragment extends Fragment {
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        database = FirebaseDatabase.getInstance();
+        binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "Try to add item", Toast.LENGTH_SHORT).show();
+                InsertItem();
             }
         });
         return root;
@@ -42,5 +54,23 @@ public class HomeFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    private void InsertItem(){
+        DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        Calendar cal = Calendar.getInstance();
+        String date = dateFormat.format(cal.getTime());
+        Data data = new Data("Keyboard", date, "1", "buy new keyboard KEKW", 400000);
+        database.getReference().child("Expenses").child("1231123").setValue(data).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(getActivity(), "Item added successfully", Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(getActivity(), "Failed to add Item", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
     }
 }
